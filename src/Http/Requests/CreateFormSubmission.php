fa7fdb8c07ad48e6bd2ad6ad942074eeaad053a4
patrick <patrick@van-ons.nl>
2025-05-02 09:44:52 +0200

    feat: Add custom validation (#2)

--- src/Http/Requests/CreateFormSubmission.php
+++ src/Http/Requests/CreateFormSubmission.php
@@ -3,6 +3,8 @@
 namespace VanOns\FilamentFormBuilder\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Route;
+use VanOns\FilamentFormBuilder\Models\Form;
 
 class CreateFormSubmission extends FormRequest
 {
@@ -11,9 +13,25 @@ class CreateFormSubmission extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'submitter_email' => ['sometimes', 'nullable', 'string'],
+        $rules = [
+            'submitter_email' => ['sometimes', 'nullable', 'string', 'email:rfc'],
             'callback_url' => ['sometimes', 'string'],
         ];
+
+        return array_merge($rules, $this->getFormRules());
+    }
+
+    private function getFormRules(): array
+    {
+        $form = Form::query()
+            ->findOrFail(Route::current()->parameter('formId'));
+
+        $rules = [];
+
+        if (method_exists($form->template, 'rules')) {
+            $rules = $form->template::rules();
+        }
+
+        return $rules;
     }
 }
