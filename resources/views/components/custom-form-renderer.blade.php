494712b25fc929c7e23e4ab6e8565ca634c57003
Kian Acquoij <kian@van-ons.nl>
2025-05-26 10:00:37 +0200

    feat(custom-forms): display success message after custom form submission

--- resources/views/components/custom-form-renderer.blade.php
+++ resources/views/components/custom-form-renderer.blade.php
@@ -3,6 +3,11 @@
 ])
 
 <form method="POST" action={{ route('filament-form-builder.form.store', ['formId' => $form->id]) }}>
+    @if (session('submit_notification_type') === 'content' && $success = session('submit_notification_content'))
+        <div class="py-4">
+            <p>{!! $success !!}</p>
+        </div>
+    @endif
     @csrf
     @foreach($fields as $field)
         @if ($type = $field['fieldType'] ?? null)
